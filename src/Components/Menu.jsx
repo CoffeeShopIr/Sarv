aedeef259ef5822b2ea74f460f7ddc0c7e447ba5
CoffeeShopIr <nohostnodamain@gmail.com>
2025-12-29 22:23:50 +0330

    2223

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -2,19 +2,16 @@ import { Link } from "react-router-dom"
 
 export default function Menu({data}){
     console.log(data)
-    function LazyImage({ src}) {
-        return <img src={src} loading="lazy"/>;
-    }
+    
     return(
         <>
             <div className="menu">
                     {data && data.map((item,i)=>{
                         return(
                             <>
-                                <div>
+                                <div style={{backgroundImage:`url(${item.Title}.jpeg)`}}>
                                     <Link to={item.Title}>
                                         <li key={i}>{item.Title}</li>
-                                        {LazyImage(item.Title+".jpeg")}
                                     </Link>
                                 </div>
                             </>
