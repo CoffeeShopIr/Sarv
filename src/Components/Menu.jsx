93bf3f10d2025b5a43f32b22f906133fe3ae9e8d
CoffeeShopIr <nohostnodamain@gmail.com>
2025-12-29 22:35:10 +0330

    2235

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom"
 
 export default function Menu({data}){
     console.log(data)
-    
     return(
         <>
             <div className="menu">
                     {data && data.map((item,i)=>{
                         return(
                             <>
-                                <div style={{backgroundImage:`url(${item.Title}.jpeg)`}}>
+                                <div>
                                     <Link to={item.Title}>
                                         <li key={i}>{item.Title}</li>
+                                        <img src={item.Title+".jpeg"} />
                                     </Link>
                                 </div>
                             </>
