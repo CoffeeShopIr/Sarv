508b3f01d60900ccaf0ac56fe238cdadb6b051d2
CoffeeShopIr <nohostnodamain@gmail.com>
2025-12-29 11:13:06 +0330

    10/08 1113

--- src/Components/DetailMenu.jsx
+++ src/Components/DetailMenu.jsx
@@ -3,14 +3,30 @@ import { useParams } from "react-router-dom"
 export default function DetailMenu({data}){
     const params=useParams()
     console.log(data)
+    
     return(
         <>
-            <h1>{params.Detailsid}</h1>
-            {data && data.map((items,i)=>{
-                return(
-                    <li>{items.Name} ------ {items.Price}</li>
-                )
-            })}
+            <section className="menu-detail">
+                
+                <div className="menu-border">
+                    <h1>{params.Detailsid}</h1>
+                    <div className="munu-detail-tilte">
+                        <h3>: قیمت</h3>
+                        <hr/>
+                        <h3>: اسم</h3>
+                    </div>
+                    <hr/>
+                    {data && data.map((items,i)=>{
+                        return(
+                            <div className="menu-detail-menu" key={i}>
+                                <li>{Number(items.Price).toLocaleString("en")}</li>
+                                <hr/>
+                                <li>{items.Name}</li>
+                            </div>
+                        )
+                    })}
+                </div>
+            </section>
         </>
     )
 }
