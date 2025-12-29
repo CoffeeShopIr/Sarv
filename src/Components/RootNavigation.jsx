3d0922f69e83443ac5419dd6e99b0cc50799ad53
CoffeeShopIr <nohostnodamain@gmail.com>
2025-12-29 16:23:33 +0330

    10-08 1623

---
+++ src/Components/RootNavigation.jsx
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+
+export default function RootNavaigation() {
+  return (
+    <>
+      <div className="Navigation">
+        <div className="NavgationGrid">
+          <div className="NavigationLink">
+            <NavLink to="" className={({isActive})=> isActive ? "action" : "none"} end >خانه</NavLink>
+          </div>
+          <div className="NavigationLink" >
+            <NavLink to="اسپرسوبار" className={({isActive})=> isActive ? "action" : "none"}>اسپرسوبار</NavLink>
+          </div>
+          <div className="NavigationLink">
+            <NavLink to="نوشیدنی گرم" className={({isActive})=> isActive ? "action" : "none"}>نوشیدنی گرم</NavLink>
+          </div>
+          <div className="NavigationLink">
+            <NavLink to="دمنوش" className={({isActive})=> isActive ? "action" : "none"}>دمنوش</NavLink>
+          </div>
+          <div className="NavigationLink">
+            <NavLink to="آیس کافی" className={({isActive})=> isActive ? "action" : "none"}>آیس کافی</NavLink>
+          </div>
+          <div className="NavigationLink">
+            <NavLink to="میلک شیک" className={({isActive})=> isActive ? "action" : "none"}>میلک شیک</NavLink>
+          </div>
+          <div className="NavigationLink">
+            <NavLink to="بار سرد" className={({isActive})=> isActive ? "action" : "none"}>بار سرد</NavLink>
+          </div>
+          <div className="NavigationLink">
+            <NavLink to="کیک" className={({isActive})=> isActive ? "action" : "none"}>کیک</NavLink>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import RootNavaigation from "../Components/RootNavigation";
 
 export default function Root(){
     return(
         <>
+        <RootNavaigation/>
         <Outlet/>
         </>
     )
