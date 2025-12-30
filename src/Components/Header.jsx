270f4920dc4b393870281e5628d1c0f353337d16
CoffeeShopIr <nohostnodamain@gmail.com>
2025-12-30 15:21:12 +0330

    1521 rahimi

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,37 @@
+import { useEffect, useState } from "react"
 
 
-export default function Header(){
-
+export default function Header({data}){
+    const [slider,setSlider]=useState(0)
+    useEffect(()=>{
+        let Time=setTimeout(()=>{
+            setSlider(prev=>{
+                let updata=prev
+                if(prev ===data.length -1){
+                    return updata=0
+                }else {
+                    return updata=updata+1
+                }
+            })
+        return ()=> clearTimeout(Time)
+        },6000)
+    },[slider])
     return(
         <>
             <header>
-                <div>
-                    <img src="دمنوش.jpeg"/>
-                </div>
-                <div>
+                <div className="centerItem">
                     <h1>Sarv</h1>
                     <p>hellow welcome to SarvCoffeShop</p>
                     <button>About</button>
+                </div>
+                <div className="backImg">
+                    {data?.map((item,index)=>{
+                        // console.log(index===slider && item)
+                        return(
+                                <img key={index} src={index===slider && item.Tittle+".jpeg"}/>
+                        )
+                    })}
+                    <img src="دمنوش.jpeg"/>
                 </div>
             </header>
         </>
