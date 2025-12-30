b27920406f6da0cb8368cf8e9862bd5715a4cebd
CoffeeShopIr <nohostnodamain@gmail.com>
2025-12-30 14:10:11 +0330

    10/09 1410 rahimi

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 export default function Menu({data}){
-    console.log(data)
+    let lastItem=""
+    if(data.length /2!==0){
+        lastItem="lastItem"
+    }
     return(
         <>
-            <div className="menu">
+            <div className={`menu ${lastItem}`}>
                     {data && data.map((item,i)=>{
                         return(
                             <>
