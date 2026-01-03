a784230fb6a93edd9a299a7d6b29dc15fc9235ec
CoffeeShopIr <nohostnodamain@gmail.com>
2026-01-03 12:45:50 +0330

    rahimi2

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from "react-router-dom";
-import { lazy } from "react";
-const DetailMenu=lazy(()=> import("../Components/DetailMenu"))
+import DetailMenu from "../Components/DetailMenu";
+
 export default function ProductDetails(){
     const data=useLoaderData()
     return(
