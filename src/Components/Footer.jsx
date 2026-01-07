7e0e8fcd80b6fee6a1d0b3afceadebaa35d78071
CoffeeShopIr <nohostnodamain@gmail.com>
2026-01-07 17:09:34 +0330

    1709 rahimi

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,15 +5,15 @@ export default function Footer(){
                 <div className="information">
                     <div>
                         <img src="instagram.png" />
-                        <p><strong>اینستاگرام: </strong><a href="instagramURL">@instaPageId</a></p>
+                        <p><strong>اینستاگرام: </strong><a href="https://www.instagram.com/sarv.cafe?igsh=MWY3aGtpdHQ3NWl3dg==">sarv.cafe</a></p>
                     </div>
                     <div>
                         <img src="phone.png"  />
-                        <p><strong>تلفن: </strong>6969 8585 021</p>
+                        <p><strong>تلفن: </strong>02165259724</p>
                     </div>
                     <div>
                         <img src="location.png" />
-                        <p><strong>آدرس:</strong> شهریار، خیابان معلم، کافه سرو</p>
+                        <p><strong>آدرس: ÷</strong>شهریار , میدان معلم , خیابان معلم , نبش کوچه تربیت بدنی , کافه سرو</p>
                     </div>
                 </div>
                 <div className="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12967.47717466306!2d51.0637806703722!3d35.6555918170873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8df3b99e59994f%3A0xea0663249921ab43!2z2qnYp9mB2Ycg2LHYs9iq2YjYsdin2YYg2LPYsdmI!5e0!3m2!1sen!2s!4v1767188072334!5m2!1sen!2s" width="100%" height="100%" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"/></div>
--- src/Components/ModalInfo.jsx
+++ src/Components/ModalInfo.jsx
@@ -5,15 +5,15 @@ export default function ModalInfo(){
             <div className="ModalInfo">
                 <div className="ModalInfoDiv">
                     <img src="instagram.png" />
-                    <a href="https://google.com">instagram</a>
+                    <a href="https://www.instagram.com/sarv.cafe?igsh=MWY3aGtpdHQ3NWl3dg==">sarv.cafe</a>
                 </div>
                 <div className="ModalInfoDiv">
                     <img src="phone.png" />
-                    <h3>09197038720</h3>
+                    <h3>02165259724</h3>
                 </div>
                 <div className="ModalInfoDiv">
                     <img src="location.png" />
-                    <p>شهریار,کوچه معلم ,کافه سرو</p>
+                    <p>شهریار , میدان معلم , خیابان معلم , نبش کوچه تربیت بدنی , کافه سرو</p>
                 </div>
             </div>
         </>
